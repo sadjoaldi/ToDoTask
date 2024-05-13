d239108f3bc7c855fca9db79b71a1c5ce22c7e7a
sadjoaldi <alsannealdi@hotmail.com>
2024-05-13 15:01:09 +0200

    creation du fichier html, des dossier js et css et des fichiers correspondants, ajout de la structure de html

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const tasks = [
+  {
+    title: "Apprendre mon cours de JavaScript",
+    priority: 1,
+  },
+  {
+    title: "Créer mon compte Github",
+    priority: 2,
+  },
+  {
+    title: "Répondre à mes emails",
+    priority: 3,
+  },
+];
